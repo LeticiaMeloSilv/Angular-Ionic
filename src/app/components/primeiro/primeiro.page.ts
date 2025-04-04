e72b06b998861239cf4dcdd7cd7c2b38a783dacc
Lehh <123740554+LeticiaMeloSilv@users.noreply.github.com>
2025-04-04 13:35:46 -0300

    Lista e Alerta

--- src/app/components/primeiro/primeiro.page.ts
+++ src/app/components/primeiro/primeiro.page.ts
@@ -5,6 +5,7 @@ import { IonicModule } from '@ionic/angular';
 import { ObservableService } from '../../service/observable/observable.service'; // Importando o serviço
 import { BehaviorSubject, interval } from 'rxjs';
 import { Router } from '@angular/router';
+// Removed standalone import of IonAlert to avoid duplicate component error
 
 @Component({
   selector: 'app-primeiro',
@@ -12,9 +13,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./primeiro.page.scss'],
   standalone: true,  //presta atenção nessa parte, pq ela ta dizendo q esse componente é standalone, ou seja, ele não precisa de um modulo pra ser chamado 
   imports: [
-    IonicModule
+    IonicModule,
+    // Removed IonAlert from imports to avoid duplicate component error
   ]
 })
+
 export class PrimeiroPage {
   dadosaq$: BehaviorSubject<any>;
   //aqui eu to criando uma variavel que vai receber os dados do observable service, e ela é do tipo Observable, pq o serviço ta retornando um observable
@@ -23,8 +26,15 @@ export class PrimeiroPage {
   constructor(private dataService: ObservableService, private storageService: StorageService, private router: Router) {// aq eu to injetando o serviço no construtor, pra poder usar ele
     //aqui eu to chamando o serviço e pegando os dados dele, e to passando pra variavel dados$
     this.dadosaq$ = new BehaviorSubject<any>(this.dataService.dados$); // Inicializa o BehaviorSubject com os dados do serviço
-  
    }
+///////////// ALERT ///////////////////////
+   isAlertOpen = false;
+   alertButtons = ['Action'];
+ 
+   setOpen(isOpen: boolean) {
+     this.isAlertOpen = isOpen;
+   }
+/////////////////// RETORNO OBSERVABLE /////////////////////////
   goToHome() {
     //essa funcao ta crianddo um novo json , em seguida ta mandando pra dentro do observable que foi definido anteriormente, e dps navega pra tela home
     const novosDados = [
@@ -41,9 +51,8 @@ export class PrimeiroPage {
     this.storageFunc()
    
     this.router.navigate(['/home']);
-
-   
   }
+/////////////////////// RETORNO STORAGE /////////////////////////////////
   storageFunc(){
     this.storageService.saveData('teste', 'teste');
     this.storageService.getData('teste').then((value) => {
